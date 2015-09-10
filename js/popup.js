6e65e4ca5bde1e8696fe343daee7bbb2a8436c07
nate <admin@local.host>
2015-09-11 01:17:03 +0800

    add popup.js, add options click function

---
+++ js/popup.js
@@ -0,0 +1,14 @@
+$(function() {
+	//设置
+	$("#options").click(function() {
+		//chrome.tabs.create({ 'url': 'chrome://extensions/?options=' + chrome.runtime.id });
+		var optionsUrl = chrome.extension.getURL('options.html');
+		chrome.tabs.query({url: optionsUrl}, function(tabs) {
+			if (tabs.length) {
+				chrome.tabs.update(tabs[0].id, {active: true});
+			} else {
+				chrome.tabs.create({url: optionsUrl});
+			}
+		});
+	});	
+});
