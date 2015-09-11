cbf258f3a6ccef0e645e8f03f1962112d3755074
nate <admin@local.host>
2015-09-11 15:05:38 +0800

    add init function

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,5 @@
 
+//打开新页面
 var open_tab = function(page) {
 	var optionsUrl = chrome.extension.getURL(page);
 	chrome.tabs.query({url: optionsUrl}, function(tabs) {
@@ -11,7 +12,40 @@ var open_tab = function(page) {
 
 };
 
+//初始化数据
+var init = function() {
+	$("#proxy").empty();
+
+	if (localStorage.getItem('proxy_data') != null) {
+		var proxy_data = JSON.parse(localStorage.getItem('proxy_data'));
+		$.each(proxy_data, function(n, data) {
+			$("#proxy").append('<a href="javascript:void(0);" class="list-group-item" value="'+data.id+'">'+data.name+'</a>');
+		});
+	}
+}
+
 $(function() {
+	//初始化数据
+	init();
+
+	//使用代理
+	$("#proxy").delegate("a", "click", function() {
+		$(this).parent().parent().find("a").prop("class", "list-group-item");
+		$(this).prop("class", "list-group-item active");
+	})
+
+	//直接连接
+	$("#direct").click(function() {
+		$(this).parent().parent().find("a").prop("class", "list-group-item");
+		$(this).prop("class", "list-group-item active");
+	});
+	
+	//使用系统代理
+	$("#system").click(function() {
+		$(this).parent().parent().find("a").prop("class", "list-group-item");
+		$(this).prop("class", "list-group-item active");
+	});
+
 	//设置
 	$("#options").click(function() {
 		open_tab('options.html');
