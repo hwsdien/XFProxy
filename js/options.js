716c72c886be8524c9e36488f5e02020d54e8afc
nate <admin@local.host>
2015-09-11 14:52:03 +0800

    add add function

--- js/options.js
+++ js/options.js
@@ -1,3 +1,14 @@
+/*
+var data = [
+	{"id": 1, "name": "日本服务器", "type": "http", "host": "127.0.0.1", "port": 6363, "rules": "*.local, 169.254/16,127.0.0.1"},
+	{"id": 2, "name": "韩国服务器", "type": "https", "host": "127.0.0.2", "port": 6463, "rules": "*.local, 169.254/16,127.0.0.2"},
+	{"id": 3, "name": "香港服务器", "type": "socks4", "host": "127.0.0.3", "port": 6563, "rules": "*.local, 169.254/16,127.0.0.3"},
+	{"id": 4, "name": "美国服务器", "type": "socks5", "host": "127.0.0.4", "port": 6663, "rules": "*.local, 169.254/16,127.0.0.4"}
+];
+
+localStorage.setItem('proxy_data', JSON.stringify(data));
+*/
+
 //初始化代理列表
 var init_list = function() {
 	if (localStorage.getItem('proxy_data') != null) {
@@ -45,33 +56,57 @@ var save = function(id, name, type, host, port, rules) {
 	if (localStorage.getItem('proxy_data') != null) {
 		var proxy_data = JSON.parse(localStorage.getItem('proxy_data'));
 		var new_proxy_data = [];
+		var flag = true;
+		var temp = {"id": parseInt(id), "name": name, "type": type, "host": host, "port": port, "rules": rules};
 		$.each(proxy_data, function(n, data) {
 			if (id == data.id) {
-				var temp = {"id": id, "name": name, "type": type, "host": host, "port": port, "rules": rules};
+				flag = false;
 				new_proxy_data.push(temp);
 			} else {
 				new_proxy_data.push(data);
 			}
 		});
+		if (flag) new_proxy_data.push(temp);
 		localStorage.setItem("proxy_data", JSON.stringify(new_proxy_data));
 		return true;
 	} else {
 		return false;
 	}
-
 };
 
-/*
+//获取新的ID
+var get_id = function() {
+	var id = 1;
+	if (localStorage.getItem('proxy_data') != null) {
+		var proxy_data = JSON.parse(localStorage.getItem('proxy_data'));
+		var id_list = [];
+		$.each(proxy_data, function(n, data) {
+			id_list.push(parseInt(data.id));
+		});
 
-var data = [
-	{"id": 1, "name": "日本服务器", "type": "http", "host": "127.0.0.1", "port": 6363, "rules": "*.local, 169.254/16,127.0.0.1"},
-	{"id": 2, "name": "韩国服务器", "type": "https", "host": "127.0.0.2", "port": 6463, "rules": "*.local, 169.254/16,127.0.0.2"},
-	{"id": 3, "name": "香港服务器", "type": "socks4", "host": "127.0.0.3", "port": 6563, "rules": "*.local, 169.254/16,127.0.0.3"},
-	{"id": 4, "name": "美国服务器", "type": "socks5", "host": "127.0.0.4", "port": 6663, "rules": "*.local, 169.254/16,127.0.0.4"}
-];
+		id_list = id_list.sort();
+		var flag = true;
 
-localStorage.setItem('proxy_data', JSON.stringify(data));
-*/
+		$.each(id_list, function(n, num) {
+			if ((n == id_list.length - 1) || (id_list[n + 1] > num + 1 )) {
+				if (flag) id = parseInt(num) + 1;
+				flag = false;
+			}
+		});
+	}
+	return id;
+}
+
+//清空
+var empty = function() {
+	$("#code").val(0);
+	$("#name").val('');
+	$("#type").find("input[type='radio']").removeAttr("checked");
+	$("#host").val('');
+	$("#port").val('');
+	$("#rules").val('');
+};
+
 
 $(function() {
 
@@ -117,5 +152,14 @@ $(function() {
 		if (save(id, name, type, host, port, rules)) {
 			init_list();
 		}
+	});
+
+	//增加
+	$("#add").delegate("", "click", function() {
+		var id = get_id();
+		var name = "新代理服务器";
+		empty();
+		$("#code").val(id);
+		$("#name").val(name);
 	});
 });
