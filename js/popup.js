a2a865d76d59a35c3f8078d7071fbe153b646f0d
nate <admin@local.host>
2015-09-11 17:07:27 +0800

    add direct, system, custom function

--- js/popup.js
+++ js/popup.js
@@ -20,9 +20,67 @@ var init = function() {
 		var proxy_data = JSON.parse(localStorage.getItem('proxy_data'));
 		$.each(proxy_data, function(n, data) {
 			$("#proxy").append('<a href="javascript:void(0);" class="list-group-item" value="'+data.id+'">'+data.name+'</a>');
+			//$("#proxy").append('<a href="javascript:void(0);" class="list-group-item">'+data.name+'</a>');
 		});
 	}
 }
+
+//直接连接
+var direct = function() {
+	var config = {
+		mode: "direct"
+	};
+	chrome.proxy.settings.set({value: config, scope: 'regular'}, function() {});
+};
+
+//使用系统代理连接
+var system = function() {
+	var config = {
+		mode: "system"
+	};
+	chrome.proxy.settings.set({value: config, scope: 'regular'}, function() {});
+};
+
+//获取数据
+var get_data = function(id) {
+	if (localStorage.getItem('proxy_data') != null) {
+		var proxy_data = JSON.parse(localStorage.getItem('proxy_data'));
+		var result = null;
+		$.each(proxy_data, function(n, data) {
+			if (id == data.id) {
+				result = data;
+			}
+		});
+		return result;
+	} else {
+		return null;
+	}
+};
+
+//使用自定义代理
+var custom = function(id) {
+	var data = get_data(id);
+	if (data == null) return false;
+
+	var host = data.host;
+	var port = data.port;
+	var type = data.type;
+	var rules = data.rules;
+
+	var config = {
+		mode: "fixed_servers",
+		rules: {
+			singleProxy: {
+				scheme: type, 
+				host: host,
+				port: port
+			},
+			bypassList: []
+		}
+	};
+	chrome.proxy.settings.set({value: config, scope: 'regular'}, function() {});
+}
+
 
 $(function() {
 	//初始化数据
@@ -32,18 +90,22 @@ $(function() {
 	$("#proxy").delegate("a", "click", function() {
 		$(this).parent().parent().find("a").prop("class", "list-group-item");
 		$(this).prop("class", "list-group-item active");
+		var id = $(this).attr("value");
+		custom(id);
 	})
 
 	//直接连接
 	$("#direct").click(function() {
 		$(this).parent().parent().find("a").prop("class", "list-group-item");
 		$(this).prop("class", "list-group-item active");
+		direct();
 	});
-	
+
 	//使用系统代理
 	$("#system").click(function() {
 		$(this).parent().parent().find("a").prop("class", "list-group-item");
 		$(this).prop("class", "list-group-item active");
+		system();
 	});
 
 	//设置
