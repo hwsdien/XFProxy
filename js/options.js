3cfe062427370df4a30125067c5aca848c7787e1
nate <admin@local.host>
2015-09-11 14:10:05 +0800

    add save function

--- js/options.js
+++ js/options.js
@@ -2,6 +2,7 @@
 var init_list = function() {
 	if (localStorage.getItem('proxy_data') != null) {
 		var proxy_data = JSON.parse(localStorage.getItem('proxy_data'));
+		$("#proxy_list").find("tr").remove();
 		$.each(proxy_data, function(n, data) {
 			$("#proxy_list").append('<tr><td><h5>'+data.name+'</h5> </td> <td>'+data.type+'</td> <td> <button type="button" class="btn btn-primary" value="'+data.id+'" name="modify">修改</button> <button type="button" class="btn btn-danger" value="'+data.id+'" name="delete">删除</button> </td> </tr>');
 		});
@@ -23,12 +24,50 @@ var delete_proxy = function(id) {
 	}
 }
 
+//获取数据
+var get_data = function(id) {
+	if (localStorage.getItem('proxy_data') != null) {
+		var proxy_data = JSON.parse(localStorage.getItem('proxy_data'));
+		var result = null;
+		$.each(proxy_data, function(n, data) {
+			if (id == data.id) {
+				result = data;
+			}
+		});
+		return result;
+	} else {
+		return null;
+	}
+};
+
+//保存数据
+var save = function(id, name, type, host, port, rules) {
+	if (localStorage.getItem('proxy_data') != null) {
+		var proxy_data = JSON.parse(localStorage.getItem('proxy_data'));
+		var new_proxy_data = [];
+		$.each(proxy_data, function(n, data) {
+			if (id == data.id) {
+				var temp = {"id": id, "name": name, "type": type, "host": host, "port": port, "rules": rules};
+				new_proxy_data.push(temp);
+			} else {
+				new_proxy_data.push(data);
+			}
+		});
+		localStorage.setItem("proxy_data", JSON.stringify(new_proxy_data));
+		return true;
+	} else {
+		return false;
+	}
+
+};
+
 /*
+
 var data = [
-	{"id": 1, "name": "日本服务器", "type": "http", "host": "127.0.0.1", "port": 6363},
-	{"id": 2, "name": "韩国服务器", "type": "http", "host": "127.0.0.1", "port": 6363},
-	{"id": 3, "name": "香港服务器", "type": "http", "host": "127.0.0.1", "port": 6363},
-	{"id": 4, "name": "美国服务器", "type": "http", "host": "127.0.0.1", "port": 6363}
+	{"id": 1, "name": "日本服务器", "type": "http", "host": "127.0.0.1", "port": 6363, "rules": "*.local, 169.254/16,127.0.0.1"},
+	{"id": 2, "name": "韩国服务器", "type": "https", "host": "127.0.0.2", "port": 6463, "rules": "*.local, 169.254/16,127.0.0.2"},
+	{"id": 3, "name": "香港服务器", "type": "socks4", "host": "127.0.0.3", "port": 6563, "rules": "*.local, 169.254/16,127.0.0.3"},
+	{"id": 4, "name": "美国服务器", "type": "socks5", "host": "127.0.0.4", "port": 6663, "rules": "*.local, 169.254/16,127.0.0.4"}
 ];
 
 localStorage.setItem('proxy_data', JSON.stringify(data));
@@ -39,9 +78,18 @@ $(function() {
 	init_list();
 
 	//修改
-	$("#proxy_list").find("button[name='modify']").click(function(){
+	$("#proxy_list").delegate("button[name='modify']", "click", function(){
 		var id = $(this).prop('value');
-		console.log(id);
+		var data = get_data(id);
+		if (data != null) {
+			$("#code").val(id);
+			$("#name").val(data.name);
+			$("#type").find("input[type='radio']").removeAttr("checked");
+			$("#type").find("input[type='radio'][value='"+data.type+"']").prop("checked", true);
+			$("#host").val(data.host);
+			$("#port").val(data.port);
+			$("#rules").val(data.rules);
+		}
 	});
 
 	//删除
@@ -51,5 +99,23 @@ $(function() {
 		$(this).parent().parent().remove();
 	});
 
+	//保存
+	$("#save").delegate("", "click", function() {
+		var id = $("#code").val();
+		var name = $("#name").val();
+		var type = null;
+		$("#type input[type='radio'][name='type']").each(function() {
+			if ($(this).prop("checked")) {
+				type = $(this).val();
+			}
+		});
 
+		var host = $("#host").val();
+		var port = $("#port").val();
+		var rules = $("#rules").val();
+
+		if (save(id, name, type, host, port, rules)) {
+			init_list();
+		}
+	});
 });
